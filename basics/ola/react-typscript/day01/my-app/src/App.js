f784a0531a5940007029c48ebd1a298e11498245
Rasaq-ola <rasaqola196@gmail.com>
2025-08-07 17:40:29 +0100

    learning input: asking the users a question and printing their reply and a message to the console

--- basics/ola/react-typscript/day01/my-app/src/App.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return "Hello, the great ".concat(name, "!");
-}
-console.log(greet("Ola"));
-exports.default = greet;
--- basics/ola/react-typscript/day01/my-app/src/App.tsx
+++
@@ -1,6 +0,0 @@
-function greet(name: string): string {
-  return `Hello, the great ${name}!`;
-}
-
-console.log(greet("Ola"));
-export default greet;
