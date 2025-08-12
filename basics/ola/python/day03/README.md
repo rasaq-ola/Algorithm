5403711644b0115b7dd2c08caef21b84567c896f
Rasaq-ola <rasaqola196@gmail.com>
2025-08-12 02:15:06 +0100

    Add Two Numbers

---
+++ basics/ola/python/day03/README.md
@@ -0,0 +1,12 @@
+# 📅 Day 3: Add Two Numbers
+
+**Difficulty:** 🌱 Easy  
+**Topics:** Variables, Input, Data Types, Arithmetic Operators
+
+---
+
+## 🧩 Problem Statement
+Write a program that:
+1. Asks the user for **two numbers**.
+2. Adds them together.
+3. Prints the result.
---
+++ basics/ola/python/day03/inputConcat.py
@@ -0,0 +1,7 @@
+firstNumber = input("Enter the first number: ")
+secondNumber = input("Enter the second number: ")
+firstNumber = int(firstNumber)
+secondNumber = int(secondNumber)
+# addition of the inputs is:
+result = firstNumber + secondNumber
+print('The addition of your numbers is:', result)
